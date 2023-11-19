97ecc1722b304a703728f20610914071076ced60
murillo dev72 <148728763+murillodev72@users.noreply.github.com>
2023-11-19 13:25:57 -0300

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CSharpShellCore;
+namespace Notas{
+	class classe{
+	static void Main(String[] args){
+	double nota1, nota2, nota3 , nota4;
+	string frase = "digite sua {0}° nota";
+	
+	
+	Console.WriteLine(frase, 1);
+	nota1 = Convert.ToDouble (Console.ReadLine() );
+	
+	Console.WriteLine(frase, 2);
+	nota2 = Convert.ToDouble (Console.ReadLine() );
+	
+	Console.WriteLine(frase, 3);
+    nota3 = Convert.ToDouble( Console.ReadLine() );
+	
+	Console.WriteLine(frase,4);
+	nota4 = Convert.ToDouble(Console.ReadLine());
+	
+	double total, media;
+	total = nota1 + nota2 + nota3 + nota4;
+	media = total / 4;
+	
+	Console.WriteLine("sua média foi:  "+ media);
+	
+	if(media < 5){
+	Console.WriteLine("você està reprovado");
+	}
+	if(media < 7){
+	Console.WriteLine("você terá que fazer recuperação");
+	}
+	else{
+	Console.WriteLine("voce esta aprovado");
+	}
+	}
+	}
+}
